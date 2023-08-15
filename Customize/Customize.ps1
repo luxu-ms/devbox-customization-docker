485eb88cbdfcb0136932bf4293245c627e187c26
Lyle Xu <chanxl009@gmail.com>
2023-08-15 18:08:47 +0800

    init

---
+++ Customize/Customize.ps1
@@ -0,0 +1,42 @@
+$newGuid = [guid]::NewGuid()
+Write-Host "new guid: $newGuid"
+$regHLMPath = 'HKLM:\Software\Microsoft\Active Setup\Installed Components'
+$tempDirectory = 'C:\Temp'
+$Name         = 'StubPath'
+
+$newPath = "$newGuid"
+$enableWSLFileName = 'Eanble-WSL2'
+$Value        = "$tempDirectory\$enableWSLFileName.lnk"
+New-Item -Path $regHLMPath -Name $newPath  -Force
+New-ItemProperty -Path "$regHLMPath\$newPath" -Name $Name -Value $Value -PropertyType String -Force
+
+$newPath2 = ">$newGuid"
+$installToolsFileName = 'Install-Docker-Desktop'
+$Value2 = "$tempDirectory\$installToolsFileName.lnk"
+New-Item -Path $regHLMPath -Name $newPath2  -Force
+New-ItemProperty -Path "$regHLMPath\$newPath2" -Name $Name -Value $Value2 -PropertyType String -Force
+
+Copy-Item -Path ".\$enableWSLFileName.ps1" -Destination $tempDirectory -Force
+Copy-Item -Path ".\$installToolsFileName.ps1" -Destination $tempDirectory -Force
+
+
+function CreateShortcutWithAdminPermission {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$shortcutName,
+        [String]$scriptFullPathName
+    )
+
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($shortcutName)
+    $Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+    $Shortcut.Arguments = "-executionpolicy remotesigned -File $scriptFullPathName"
+    $Shortcut.Save()
+
+    $bytes = [System.IO.File]::ReadAllBytes($shortcutName)
+    $bytes[0x15] = $bytes[0x15] -bor 0x20
+    [System.IO.File]::WriteAllBytes($shortcutName, $bytes)
+}
+
+CreateShortcutWithAdminPermission -shortcutName $Value -scriptFullPathName "$tempDirectory\$enableWSLFileName.ps1"
+CreateShortcutWithAdminPermission -shortcutName $Value2 -scriptFullPathName "$tempDirectory\$installToolsFileName.ps1"
---
+++ Customize/Eanble-WSL2.ps1
@@ -0,0 +1,2 @@
+wsl --install
+Restart-Computer
